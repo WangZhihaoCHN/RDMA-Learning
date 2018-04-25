2f086e2d7c36322af7fef10e560e07de0622bf44
WangZhihao <wangzhihao.chn@gmail.com>
2018-04-25 15:12:46 +0800

    MPI通信重叠测试程序

--- Glex/mpi_overlapping.c
+++ Glex/mpi_overlapping.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include "mpi.h"
 
-#define SIZE 40000
+#define SIZE 1000
 
 int main(int argc, char *argv[])
 {
@@ -20,64 +20,43 @@ int main(int argc, char *argv[])
 	MPI_Request handle;			//MPI请求(request)
 
 	//记录MPI_Isend()开始时间，cpu占用结束时间，MPI_Wait()结束时间
-	double startTime,cpuTime,waitTime,totalTime;		
+	double startTime,cpuTime,endTime,totalTime;		
 
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD,&num_procs);
 	MPI_Comm_rank(MPI_COMM_WORLD,&my_id);
 
+
+	// 同步
+	MPI_Barrier(MPI_COMM_WORLD); 
+	// 开始计时 
+	startTime = MPI_Wtime();
 	//两个进程，进程1非阻塞发送，进程0阻塞接收
 	if(my_id == 1){
-		// 同步
-		MPI_Barrier(MPI_COMM_WORLD); 
-
-		// 开始发送，计时 
-		startTime = MPI_Wtime();
 
 		// MPI异步发送
 		MPI_Isend(buf, SIZE*SIZE, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &handle);
-		printf("发送进程: 开始发送，占用CPU %.2lf个单位时间\n", SEC);
-
-		/*clock_t st, rt;				//占用cpu开始时间，占用cpu当前执行时间
-		st = clock();
-		double cps = (double)(SEC * CLOCKS_PER_SEC);  //需要占用SEC秒数
-		while(1){
-			rt = clock();
-			if( (double)(rt-st) >= cps)
-				break;
-		}*/
 
 		//占用CPU一段时间（SEC单位时间）
 		volatile double timer = 0;
-		int i=0;
-		for(i=0; i<SEC*10000000; i++)
+		long i=0;
+		for(i=0; i<SEC*1000; i++)
 			timer += i*i*2;
 		
 		cpuTime = MPI_Wtime();
-		printf("发送进程: CPU占用完毕，用时 %lf 秒\n", cpuTime-startTime);
 
 		//调用MPI_Wait等待异步发送结束，记录时间
 		MPI_Wait(&handle,&status);
-
-		// 同步
-		MPI_Barrier(MPI_COMM_WORLD); 
-
-		endTime = MPI_Wtime();
-		//printf("Sending task finished : %lf seconds \n", waitTime);
-
-		totalTime = endTime - startTime;
-		printf("发送进程: 已经完成，用时 %.4lf 秒\n", totalTime);
-
 	}else if(my_id == 0){
-		// 同步
-		MPI_Barrier(MPI_COMM_WORLD); 
-
 		//阻塞接受
 		MPI_Recv(buf, SIZE*SIZE, MPI_CHAR, 1, 0, MPI_COMM_WORLD, &status);
-
-		// 同步
-		MPI_Barrier(MPI_COMM_WORLD); 
 	}
+	// 同步
+	MPI_Barrier(MPI_COMM_WORLD); 
+	// 结束计时
+	endTime = MPI_Wtime();
+	totalTime = endTime - startTime;
+	printf("发送进程: 已经完成，用时 %.4lf 秒\n", totalTime);
 	
 	MPI_Finalize();
 	return 0;
